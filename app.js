69ccbc0a5e8b5c012face1306619da33ec1348de
viktorortelan <jvortelan@gmail.com>
2024-08-02 00:34:08 -0300

    atualizações

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("ola")
+
+import express from 'express';
+
+const servidor = express();
+
+servidor.get('/helloworld', (req, resp) => {
+    //codigo do endpoint
+    resp.send('hello world')
+});
+
+servidor.get('/mensagem/boasvindas', (req, resp) => {
+    resp.send('ola, sejam bem-vindos')
+})
+
+servidor.get('/v2/mensagem/boasvindas', (req, resp) => {
+    resp.send('ola, sejam bem-vindos usuarios de iphone')
+})
+
+servidor.listen(5001, () => console.log('api subida com sucesso'));
